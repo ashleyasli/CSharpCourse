e604a68e41ba95a18f6db9f4e718f808833dc2f2
Ashley Saylik <ashley.saylik@tetratech.com>
2019-08-30 15:19:24 -0500

    Added AccessModifiers and AccessModifiersDemo

---
+++ AccessModifiersDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using AccessModifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Course course = new Course();
+        }
+    }
+}
