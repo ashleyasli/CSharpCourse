c6d83637eefe85efb83244fc933bd9510378fd37
Ashley Saylik <ashley.saylik@tetratech.com>
2019-09-03 17:08:37 -0500

    Added AdoNetDemo - initial setup

---
+++ AdoNetDemo/ProductDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        public List<Product> GetAll()
+        { SqlConnection _connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+        if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }        
+    }        
+}
