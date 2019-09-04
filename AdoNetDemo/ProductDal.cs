b0344c7c8d03eb96fae49db9f60b754881ab5d09
Ashley Saylik <ashley.saylik@tetratech.com>
2019-09-04 09:18:23 -0500

    AdoNetDemo - Listing Data 2

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -10,9 +10,39 @@ namespace AdoNetDemo
 {
     public class ProductDal
     {
-        public DataTable GetAll()
+        public List<Product> GetAll()
         { SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
         if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Products", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                };
+                products.Add(product);
+            }
+
+            reader.Close();
+            connection.Close();
+            return products;
+        }
+
+        public DataTable GetAll2()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade;integrated security=true");
+            if (connection.State == ConnectionState.Closed)
             {
                 connection.Open();
             }
@@ -25,6 +55,6 @@ namespace AdoNetDemo
             reader.Close();
             connection.Close();
             return dataTable;
-        }        
+        }
     }        
 }
